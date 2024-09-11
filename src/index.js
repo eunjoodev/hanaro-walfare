ae1d3c17b27ef42c5ddede182e1462e33e7f08aa
eunjoodev <eunjoo.dev@gmail.com>
2024-09-12 02:24:12 +0900

    design: 프로젝트 전체 글꼴 설정

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { RecoilRoot } from "recoil";
+import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
