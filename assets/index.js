73d76fa22b521a534eb7789c0242dbd0cd3ea6f1
eunjoodev <eunjoo.dev@gmail.com>
2024-09-14 00:11:16 +0900

    Updates

---
+++ assets/index.js
@@ -0,0 +1,17 @@
+export { default as logo } from "./assets.logo";
+export { default as gridIcon } from "./assets.gridIcon";
+export { default as listIcon } from "./assets.listIcon";
+export { default as youtube } from "./assets.youtube";
+export { default as twitter } from "./assets.twitter";
+export { default as instagram } from "./assets.instagram";
+export { default as facebook } from "./assets.facebook";
+export { default as blog } from "./assets.blog";
+export { default as caretDown } from "./assets.caretDown";
+export { default as magnifyingGlass } from "./assets.magnifyingGlass";
+export { default as userPlus } from "./assets.userPlus";
+export { default as downloadSimple } from "./assets.downloadSimple";
+export { default as favicon } from "./assets.favicon";
+export { default as board } from "./assets.ico_docu";
+export { default as CaretLeft } from "./assets.CaretLeft";
+export { default as CaretRight } from "./assets.CaretRight";
+export { default as Rows } from "./assets.Rows";
