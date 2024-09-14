ad9675dd82055aca05d9449de9b30070e414c025
fatalism31127 <fatalism3872@proton.me>
2024-09-14 20:36:17 +0900

    feature:hyojun 배포 실험

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 const allowedOrigins = [
-  "https://hanaro-walfare.vercel.app",
+  "https://hanaro-walfare1.vercel.app",
 ];
 
 const corsOptions = {
