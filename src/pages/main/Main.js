4a937906623099e4595016cdb23638cbdaf1637c
fatalism31127 <fatalism3872@proton.me>
2024-09-14 21:48:16 +0900

    feature:vercel test2

--- src/pages/main/Main.js
+++ src/pages/main/Main.js
@@ -1,35 +1,20 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import MainBanner from './MainBanner';
-import KeyBenefits from './KeyBenefits';
-import QuickFind from './QuickFind';
-import WelfareList from './WelfareList';
-import ChatWidget from './ChatWidget';
+import React from "react";
+import MainBanner from "./MainBanner";
+import KeyBenefits from "./KeyBenefits";
+import QuickFind from "./QuickFind";
+import FilterComponent from "./FilterComponent";
+import WelfareList from "./WelfareList";
+import ChatWidget from "./ChatWidget";
 
 const Main = () => {
-  const [data, setData] = useState(null);
-
-  const apiUrl = process.env.REACT_APP_API_URL || '/api';
-  
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(`${apiUrl}/endpoint`);
-        setData(response.data);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
-    fetchData();
-  }, [apiUrl]);
-
   return (
     <>
       <main>
         <MainBanner />
         <KeyBenefits />
         <QuickFind />
-        <WelfareList data={data} />
+        {/* <FilterComponent /> */}
+        <WelfareList />
         <ChatWidget />
       </main>
     </>
