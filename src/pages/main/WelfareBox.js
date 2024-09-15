daed4b857dea6645a34b539a4552e8c20571fef2
eunjoodev <eunjoo.dev@gmail.com>
2024-09-15 14:16:11 +0900

    design: 메인페이지 리스트 디자인 초안

--- src/pages/main/WelfareBox.js
+++ src/pages/main/WelfareBox.js
@@ -4,12 +4,19 @@ import styles from "./WelfareBox.module.css";
 const WelfareBox = ({ item, layout }) => {
   return (
     <div className={layout === "grid" ? styles.gridItem : styles.listItem}>
-      <h3>{item.serviceName}</h3>
-      <p>분야: {item.serviceField}</p>
-      <p>기관: {item.supervisingAgencyName}</p>
-      <p>신청 기한: {item.applicationDeadline}</p>
-      <p>신청 방법: {item.applicationMethod}</p>
-      <p>설명: {item.servicePurposeSummary}</p>
+      <div className={styles.contentContainer}>
+        <div className={styles.tag}>{item.supervisingAgencyName}</div>
+        <div className={styles.title}>{item.serviceName}</div>
+        <div className={styles.summary}>{item.servicePurposeSummary}</div>
+        <div className={styles.fieldTag}>{item.serviceField}</div>
+        <div className={styles.applicationDead}>
+          신청 기한: {item.applicationDeadline}
+        </div>
+        <div className={styles.applicationWay}>
+          신청 방법: {item.applicationMethod}
+        </div>
+      </div>
+      <button className={styles.detailButton}>상세보기</button>
     </div>
   );
 };
