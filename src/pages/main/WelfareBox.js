932c10ca62eb340a0d91b3ab1c74db4beef12c6d
eunjoodev <eunjoo.dev@gmail.com>
2024-09-18 20:23:38 +0900

    design: 메인 페이지 리스트 ui 초안

--- src/pages/main/WelfareBox.js
+++ src/pages/main/WelfareBox.js
@@ -7,26 +7,34 @@ const WelfareBox = ({ item, layout, index }) => {
 
   const handleDetailClick = () => {
     if (item.serviceName) {
-      const encodedServiceName = encodeURIComponent(item.serviceName.replace(/\s/g, ''));
+      const encodedServiceName = encodeURIComponent(
+        item.serviceName.replace(/\s/g, "")
+      );
       navigate(`/welfare/detail/${encodedServiceName}`);
     } else {
       console.error("Service name is missing");
     }
   };
-  
-  
+
   return (
-    <div key={`welfare-${index}`} className={layout === "grid" ? styles.gridItem : styles.listItem}>
+    <div
+      key={`welfare-${index}`}
+      className={layout === "grid" ? styles.gridItem : styles.listItem}
+    >
       <div className={styles.contentContainer}>
-        <div className={styles.tag}>{item.supervisingAgencyName}</div>
-        <div className={styles.title}>{item.serviceName}</div>
-        <div className={styles.summary}>{item.servicePurposeSummary}</div>
-        <div className={styles.fieldTag}>{item.serviceField}</div>
-        <div className={styles.applicationDead}>
-          신청 기한: {item.applicationDeadline}
+        <div className={styles.tagAndTitle}>
+          <div className={styles.tag}>{item.supervisingAgencyName}</div>
+          <div className={styles.title}>{item.serviceName}</div>
         </div>
-        <div className={styles.applicationWay}>
-          신청 방법: {item.applicationMethod}
+        <div className={styles.details}>
+          <div className={styles.summary}>{item.servicePurposeSummary}</div>
+          <div className={styles.fieldTag}>{item.serviceField}</div>
+          <div className={styles.applicationDead}>
+            신청 기한: {item.applicationDeadline}
+          </div>
+          <div className={styles.applicationWay}>
+            신청 방법: {item.applicationMethod}
+          </div>
         </div>
       </div>
       <button className={styles.detailButton} onClick={handleDetailClick}>
