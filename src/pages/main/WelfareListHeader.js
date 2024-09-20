8f4ba7e00f216a5cce6f37dcf9fe55c93abed68c
eunjoodev <eunjoo.dev@gmail.com>
2024-09-20 18:20:13 +0900

    design: 메인 페이지 리스트헤더 tooltip 구현

--- src/pages/main/WelfareListHeader.js
+++ src/pages/main/WelfareListHeader.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./WelfareListHeader.module.css";
 
 const WelfareListHeader = ({ totalItems, layout, setLayout }) => {
+  const [showTooltip, setShowTooltip] = useState(true);
+
+  const handleCloseTooltip = () => {
+    setShowTooltip(false);
+  };
+
   return (
     <div className={styles.headerContainer}>
       <div className={styles.totalItems}>총 게시물: {totalItems}개</div>
+
+      {showTooltip && (
+        <div className={styles.tooltip}>
+          <p>보기 편한 방식을 선택해 보세요!</p>
+          <button className={styles.closeButton} onClick={handleCloseTooltip}>
+            X
+          </button>
+        </div>
+      )}
 
       <div className={styles.controls}>
         <label>
